bb9463fd9d16a5fa6e0c5658d89774373e4d24d9
Alan Beck <beckalan303@gmail.com>
2018-05-02 16:10:32 -0300

    se sube el form de sucursal

--- apps/perfiles/urls.py
+++ apps/perfiles/urls.py
@@ -2,10 +2,11 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from .views import UserCreateView
+from .views import UserCreateView, PerfilCreateView
 
 
 urlpatterns = [
    # Urls comunes
-   url(r'^alta/$', UserCreateView.as_view(), name='perfiles-alta')
+   url(r'^alta/$', UserCreateView.as_view(), name='perfiles-alta'),
+   url(r'^alta/sucursal/$', PerfilCreateView.as_view(), name='perfil-sucursal-alta')
 ]
--- apps/perfiles/views.py
+++ apps/perfiles/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView
 
+from .forms import PerfilForm
+from .models import Perfil
+
 
 class UserCreateView(CreateView):
 
@@ -15,4 +18,17 @@ class UserCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super(UserCreateView, self).get_context_data(**kwargs)
         context['usuarios'] = User.objects.all()
+        return context
+
+
+class PerfilCreateView(CreateView):
+
+    model = Perfil
+    form_class = PerfilForm
+    template_name = 'perfiles/perfil_sucursal_form.html'
+    success_url = '/perfiles/alta/sucursal'
+
+    def get_context_data(self, **kwargs):
+        context = super(PerfilCreateView, self).get_context_data(**kwargs)
+        context['perfiles'] = Perfil.objects.all()
         return context
