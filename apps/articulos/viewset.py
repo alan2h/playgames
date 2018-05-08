cb40eb93abb79868c3774c94ae9716f664c62f4e
Alan Beck <beckalan303@gmail.com>
2018-05-07 23:47:45 -0300

    division de articulos por sucursal en ventas

--- apps/articulos/viewset.py
+++ apps/articulos/viewset.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Articulo
 from .serializer import ArticuloSerializer
 
+from apps.perfiles.models import Perfil
+
 
 class ArticuloPagination(PageNumberPagination):
     page_size = 10
@@ -18,28 +20,36 @@ class ArticuloViewSet(ModelViewSet):
     pagination_class = ArticuloPagination
 
     def get_queryset(self):
-
-        queryset = Articulo.objects.filter(baja=False)
+        id_sucursal = 0
+        if self.request.user.is_staff:
+            id_sucursal = self.request.session['id_sucursal']
+        else:
+            perfil = Perfil.objects.filter(usuario=self.request.user.id)[0]
+            id_sucursal = perfil.sucursal.id
+        queryset = Articulo.objects.filter(baja=False, sucursal=id_sucursal)
         descripcion = self.request.query_params.get('descripcion', None)
         if descripcion is not None:
             queryset = Articulo.objects.filter(nombre__icontains=
-                                               descripcion, baja=False)
+                                               descripcion, baja=False, sucursal=id_sucursal)
             if queryset.exists() is False:
                 queryset = Articulo.objects.filter(codigo_barra__icontains=
-                                                   descripcion, baja=False)
+                                                   descripcion, baja=False, sucursal=id_sucursal)
                 if queryset.exists() is False:
                     queryset = Articulo.objects.filter(
                         rubro__descripcion__icontains=descripcion,
-                        baja=False
+                        baja=False,
+                        sucursal=id_sucursal
                     )
                     if queryset.exists() is False:
                         queryset = Articulo.objects.filter(
                             marca__descripcion__icontains=descripcion,
-                            baja=False
+                            baja=False,
+                            sucursal=id_sucursal
                         )
                         if queryset.exists() is False:
                             queryset = Articulo.objects.filter(
-                            descripcion__icontains=descripcion,
-                            baja=False
-                        )
+                                descripcion__icontains=descripcion,
+                                baja=False,
+                                sucursal=id_sucursal
+                            )
         return queryset
