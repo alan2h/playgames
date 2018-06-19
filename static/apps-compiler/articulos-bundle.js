9bc1a8295008384fe1dbf09e853fa7049e4edc27
Alan Beck <beckalan303@gmail.com>
2018-06-19 20:32:33 -0300

    se sube muchos cambios configurados para las ventas

--- static/apps-compiler/articulos-bundle.js
+++ static/apps-compiler/articulos-bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "4079023ab872f372260d"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "e80a17af263fb048adbb"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
